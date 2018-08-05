ca20b4c3dc29c7bc24d8b842b68bcb95f7c62fb9
Jordan Binskin <jordan.binskin@gmail.com>
2018-08-06 08:13:00 +1000

    my first session

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: "Jordan's site",
   },
   plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [`gatsby-plugin-sass`]
 }
